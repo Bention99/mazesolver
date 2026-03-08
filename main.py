081b8bea260a4fe8ebb0df202ae0eb01dd3d508e
Bention99 <benoitwyss0@gmail.com>
2026-03-08 15:34:35 +0100

    draw lines to window

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 def main():
     win = Window(800, 600)
+    line = Line(Point(10, 10), Point(300, 300))
+    win.draw_line(line, "black")
+    line = Line(Point(50, 100), Point(20, 300))
+    win.draw_line(line, "blue")
     win.wait_for_close()
 
 
